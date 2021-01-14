99488a50a1e041954497a0620e585a35fd67fc10
Christian Pereira <christianpbsd@gmail.com>
2021-01-14 20:19:10 -0300

    Styled App

---
+++ src/AppStyled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    background-color: #FF0000; 
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    background-color: #00FF00;
+    width: 80px; 
+`;
+
+export const PageBody = styled.div`
+    display: flex;
+    background-color: #0000FF;
+    flex: 1; 
+`;
