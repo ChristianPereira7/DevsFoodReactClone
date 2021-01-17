990f43b889aeb6633242ca1032e4cdaa93220931
Christian Pereira <christianpbsd@gmail.com>
2021-01-17 16:28:31 -0300

    Add categories

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,29 +1,51 @@
 import React, {useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
-import { Container, Titulo } from './styled';
+import { Container, CategoryArea, CategoryList } from './styled';
 
 import Header from '../../components/Header';
 
 import api from '../../api';
 
+import CategoryItem from '../../components/CategoryItem';
 
 export default () => {
 
+    const history = useHistory();
+
     const [headerSearch, setHeaderSearch] = useState('');
 
+    const [categories, setCategories ] = useState([]);
     useEffect(() => {
         const getCategories = async() => {
-        const categories = await api.getCategories();
-        console.log(categories);
+        const cat = await api.getCategories();
+        if(cat.error  == ''){
+            setCategories( cat.result );
+        }
         };
         getCategories();
     }, []);
 
-    const history = useHistory();
+   
 
     return (
         <Container>
             <Header search={headerSearch} onSearch={setHeaderSearch}/>
+       
+            {categories.length > 0 &&
+
+                    <CategoryArea>
+                        Selecione uma categoria
+                        
+                        <CategoryList>
+
+                            <CategoryItem title="Todas as categorias" image="food-and-restaurant.png"/>
+
+                        </CategoryList>
+                
+                </CategoryArea>
+                
+            }
+
         </Container>
     );
 }
