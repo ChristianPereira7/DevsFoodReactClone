cd5e7bffb0d897aced1836dfaefe3205d483caf3
Christian Pereira <christianpbsd@gmail.com>
2021-01-16 23:30:16 -0300

    Api config

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import { Container, Titulo } from './styled';
 
 import Header from '../../components/Header';
 
+import api from '../../api';
+
+
 export default () => {
 
     const [headerSearch, setHeaderSearch] = useState('');
 
+    useEffect(() => {
+        const getCategories = async() => {
+        const categories = await api.getCategories();
+        console.log(categories);
+        };
+        getCategories();
+    }, []);
 
     const history = useHistory();
 
