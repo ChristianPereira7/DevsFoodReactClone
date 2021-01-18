f302400c86c13332140d09a0d47de010eb633407
Christian Pereira <christianpbsd@gmail.com>
2021-01-18 13:40:47 -0300

    request api info of products

--- src/components/ProductItem/styled.js
+++ src/components/ProductItem/styled.js
@@ -3,3 +3,45 @@ import styled from 'styled-components';
 export const Container = styled.div`
 
 `;
+  
+/*
+ ProductPhotoArea,
+        ProductInfoArea,
+        ProductButtonArea,
+        ProductPhoto,
+        ProductName,
+        ProductPrice,
+        ProductIngredients,
+        ProductButton
+*/
+
+export const ProductPhotoArea = styled.div`
+
+`;
+export const ProductInfoArea = styled.div`
+
+`;
+
+export const ProductButtonArea = styled.div`
+
+`;
+
+export const ProductPhoto = styled.img`
+
+`;
+
+export const ProductName = styled.div`
+
+`;
+
+export const ProductPrice = styled.div`
+
+`;
+
+export const ProductIngredients = styled.div`
+
+`;
+
+export const ProductButton = styled.img`
+
+`;
